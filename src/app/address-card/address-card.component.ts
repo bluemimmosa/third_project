a6419e210ed8b7b2c95d34e04b0351925e409e23
Niraj Khakda <console1913@gmail.com>
2020-02-20 07:24:10 +0545

    passing member variables to components

--- src/app/address-card/address-card.component.ts
+++ src/app/address-card/address-card.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { User } from './user.model';
 
 @Component({
   selector: 'app-address-card',
@@ -6,24 +7,14 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./address-card.component.css']
 })
 export class AddressCardComponent implements OnInit {
-	user: any;
-	@Input('name') userName: string; //@Input is only called after constructor is initialized.
+	@Input('user') user: User; //@Input is only called after constructor is initialized.
   constructor() { //constructor code
   	// called when the selector is used.
   	//doesnt initialize all fields.
   }
 
   ngOnInit(): void { //ngOnInit() is called after complete intialization of object is finished.
-  	this.user = {
-		name: this.userName,
-		title: 'Software Developer',
-		address: 'Indrayani, Shankharapur-09, Kathmandu, Nepal',
-		phone:[
-			'977 9840014714',
-			'977 9843059542',
-			'977 9808229804'
-		],
-	};
+  
   }
 
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from './address-card/user.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  
+  user: User;
+  constructor() {
+    this.user = new User();
+    this.user.name = "Niraj Khadka";
+    this.user.designation = "Software Tester";
+    this.user.address = "Shankharapur-09, Indrayani, KAthmandu";
+    this.user.phone = [
+      '9808229804',
+      '9843059542',
+      '9840014714'
+    ];
+  }
 }
